50f2504f8e593d7dc77b30b027422614061f7c34
Saylee <saylee.waje@gmail.com>
2024-05-04 21:40:35 +0530

    commit

--- TravelRecommendationApp/travel_recommendation.js
+++ TravelRecommendationApp/travel_recommendation.js
@@ -1,4 +1,5 @@
 const btnSearch = document.getElementById('btnSearch');
+btnSearch.addEventListener('click', searchCondition);
 
 
 function searchCondition() {
@@ -6,29 +7,6 @@ function searchCondition() {
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
-    fetch('travel_recommendation_api.json')
-        .then(response => response.json())
-        .then(data => {
-            const countries = data.countries.find(item => item.name.toLowerCase() === input);
+    console.log(fetch('travel_recommendation_api.json'));
 
-            if (countries) {
-                const cities = countries.cities.join(', ');
-                const temples = countries.temples.join(', ');
-                const beaches = countries.beaches;
-
-                resultDiv.innerHTML += `<h2>${countries.name}</h2>`;
-                resultDiv.innerHTML += `<img src="${countries.imageUrl}" alt="hjh">`;
-
-                resultDiv.innerHTML += `<p><strong>Cities:</strong> ${cities}</p>`;
-                resultDiv.innerHTML += `<p><strong>Temples:</strong> ${temples}</p>`;
-                resultDiv.innerHTML += `<p><strong>Beaches:</strong> ${beaches}</p>`;
-            } else {
-                resultDiv.innerHTML = 'Condition not found.';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            resultDiv.innerHTML = 'An error occurred while fetching data.';
-        });
 }
-btnSearch.addEventListener('click', searchCondition);
